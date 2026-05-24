aad7056a3e93a532a5b00cd106d9eddbc45bba82
jwray1130 <justin.wray42@gmail.com>
2026-05-24 13:40:17 -0400

    Add files via upload

--- workbench-app.js
+++ workbench-app.js
@@ -5,7 +5,7 @@
 =====================================================================
 */
 
-window.STM_BUILD = 'v8.7.61-phase7-final-polish-2026-05-24';
+window.STM_BUILD = 'v8.7.62-fixes-word-break-counter-compression-flow-2026-05-24';
 console.log('[STM BUILD]', window.STM_BUILD);
 
 document.addEventListener('DOMContentLoaded', () => {
