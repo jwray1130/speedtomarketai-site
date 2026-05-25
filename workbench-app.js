58c42350be7214ee4dc9e82d536bf9b32337bad5
jwray1130 <justin.wray42@gmail.com>
2026-05-24 22:01:59 -0400

    Add files via upload

--- workbench-app.js
+++ workbench-app.js
@@ -5,7 +5,7 @@
 =====================================================================
 */
 
-window.STM_BUILD = 'v8.7.74-file-manager-thumbnail-strip-2026-05-24';
+window.STM_BUILD = 'v8.7.75-showcase-row-gap-2026-05-25';
 console.log('[STM BUILD]', window.STM_BUILD);
 
 document.addEventListener('DOMContentLoaded', () => {
