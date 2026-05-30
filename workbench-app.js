7d6fad5a09683efc59f8044cf33a77647e4958ba
jwray1130 <justin.wray42@gmail.com>
2026-05-29 22:40:10 -0400

    Add files via upload

--- workbench-app.js
+++ workbench-app.js
@@ -5,7 +5,7 @@
 =====================================================================
 */
 
-window.STM_BUILD = 'v8.7.85-relabel-clobber-auth-redirect-fix-2026-05-28';
+window.STM_BUILD = 'v8.7.53-manual-doc-label-persist-2026-05-24';
 console.log('[STM BUILD]', window.STM_BUILD);
 
 document.addEventListener('DOMContentLoaded', () => {
